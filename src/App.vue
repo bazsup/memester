2c50dc19ca626583a8b166ec5d11cf8c554822d0
bazsup <supawit.ruen@mail.kmutt.ac.th>
2018-11-10 23:38:19 +0700

    [UPDATE] App render Navbar and Sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,36 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <Navbar />
+    <section class="main-content columns is-fullheight is-marginless">
+      <Sidebar />
+      <div class="column is-2 is-narrow-mobile is-fullheight section is-hidden-mobile"/>
+      <div class="container column is-9" style="padding: 65px 0.75rem;">
+        <router-view/>
+      </div>
+    </section>
   </div>
 </template>
 
-<style>
+<script>
+import Navbar from '@/components/Navbar.vue'
+import Sidebar from '@/components/Sidebar.vue'
+
+export default {
+  components: {
+    Navbar,
+    Sidebar
+  }
+}
+</script>
+
+<style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
   color: #2c3e50;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+@import "~bulma/bulma";
+@import "~bulma-extensions/bulma-slider/dist/css/bulma-slider";
 </style>
