5703cb2b183d1682a2bfc96719795f6c009005b4
bazsup <supawit.ruen@mail.kmutt.ac.th>
2018-11-10 23:35:04 +0700

    [ADD] service/memeapi

---
+++ src/service/memeapi.js
@@ -0,0 +1,34 @@
+import axios from 'axios'
+
+const memeEndpoint = 'http://version1.api.memegenerator.net/'
+
+export default {
+  getRandomMeme () {
+    return axios
+      .get('https://api.icndb.com/jokes/random')
+      .then(response => response.data)
+  },
+  getByPopular (page = 0) {
+    return axios({
+      baseURL: memeEndpoint,
+      url: 'Generators_Select_ByPopular',
+      params: {
+        pageIndex: page,
+        apiKey: 'demo'
+      }
+    })
+      .then(response => response.data.result)
+  },
+  getByKeyword (word = '', page) {
+    return axios({
+      baseURL: memeEndpoint,
+      url: 'Generators_Search',
+      params: {
+        q: word,
+        pageIndex: page,
+        apiKey: 'demo'
+      }
+    })
+      .then(response => response.data.result)
+  }
+}
