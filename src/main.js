2a369d9e01a17cfe01dc44023c7b703dc54dc7e2
bazsup <supawit.ruen@mail.kmutt.ac.th>
2018-11-10 23:30:41 +0700

    [UPDATE] main.js inject font-awesome component

--- src/main.js
+++ src/main.js
@@ -3,6 +3,15 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faImage, faHome } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faImage)
+library.add(faHome)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 Vue.config.productionTip = false
 
 new Vue({
