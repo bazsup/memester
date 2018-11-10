0ec1c62e28acd324397dce712430897434bb2a20
bazsup <supawit.ruen@mail.kmutt.ac.th>
2018-11-10 23:40:47 +0700

    [ADD] views Generator

---
+++ src/views/Generator.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="meme-generator">
+    <MemeGeneratorForm />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import MemeGeneratorForm from '@/components/MemeGeneratorForm.vue'
+
+export default {
+  name: 'home',
+  components: {
+    MemeGeneratorForm
+  }
+}
+</script>
