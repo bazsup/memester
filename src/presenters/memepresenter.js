0307c931a7ec1579a8fe9feda71b3ba73d083607
bazsup <supawit.ruen@mail.kmutt.ac.th>
2018-11-10 23:38:52 +0700

    [ADD] presenter for memelist

---
+++ src/presenters/memepresenter.js
@@ -0,0 +1,32 @@
+import MemeAPI from '@/service/memeapi'
+
+export default class {
+  constructor (view) {
+    this.view = view
+    this.joke = ''
+    this.page = 0
+    this.memes = []
+    this.keyword = ''
+    this.loading = true
+  }
+  async init () {
+    for (let i = 0; i < 5; i++) {
+      this.memes = this.memes.concat(await MemeAPI.getByPopular(this.page))
+      this.page += 1
+    }
+    this.loading = false
+  }
+  searchByKeyword = async () => {
+    this.loading = true
+    this.page = 0
+    this.memes = []
+    if (!this.keyword) {
+      return this.init()
+    }
+    for (let i = 0; i < 5; i++) {
+      this.memes = this.memes.concat(await MemeAPI.getByKeyword(this.keyword, 0))
+      this.page += 1
+    }
+    this.loading = false
+  }
+}
