76082ab1c2f46fa51afbbaedcb3b8667a96c7876
bazsup <supawit.ruen@mail.kmutt.ac.th>
2018-11-11 09:27:25 +0700

    [ADD] style for loading-spinner, input search with add-on

--- src/components/MemeList.vue
+++ src/components/MemeList.vue
@@ -3,17 +3,29 @@
     <div class="columns">
       <div class="column">
         <h1 class="title">Meme list</h1>
-        <input
-          type="text"
-          v-model="presenter.keyword"
-          @keydown.enter="presenter.searchByKeyword"
-          class="input is-medium"
-          placeholder="Search.."
-        />
+        <form v-on:submit.prevent="presenter.searchByKeyword">
+          <div class="field has-addons">
+            <div class="control is-expanded">
+              <div class="control">
+                <input
+                  type="text"
+                  name="keyword"
+                  v-model="presenter.keyword"
+                  class="input is-medium"
+                  placeholder="Search..."
+                />
+              </div>
+            </div>
+            <div class="control">
+              <button type="submit" class="button is-medium custom-button">Search</button>
+            </div>
+          </div>
+        </form>
       </div>
     </div>
-    <div v-if="presenter.loading" class="has-text-centered">
-      Loading...
+    <div v-if="presenter.loading" class="has-text-centered loading">
+      <font-awesome-icon icon="spinner" class="loading-spinner" />
+      <div>Loading</div>
     </div>
     <div v-else-if="!presenter.memes.length">Result not found</div>
     <div v-else class="columns is-multiline custom-columns-layout">
@@ -73,4 +85,32 @@ export default {
   }
 }
 
+@keyframes spin {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+
+.loading {
+  color: #477FB0;
+  margin-top: 60px;
+
+  .loading-spinner {
+    animation: spin 1.5s infinite linear;
+    margin-bottom: 10px;
+    font-size: 60px;
+  }
+}
+
+.custom-button {
+  background: #4d8ac0;
+  color: #fff;
+
+  &:hover {
+    background: #477dae;
+  }
+}
 </style>
