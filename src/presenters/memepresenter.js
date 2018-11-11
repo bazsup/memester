d49b4828cd3d0b8ebb7018aea19f9dc3fb4bf9d6
bazsup <supawit.ruen@mail.kmutt.ac.th>
2018-11-11 09:26:48 +0700

    [UPDATE] meme presenter refactor init, searchByKeyword function

--- src/presenters/memepresenter.js
+++ src/presenters/memepresenter.js
@@ -4,34 +4,23 @@ export default class {
   constructor (view) {
     this.view = view
     this.joke = ''
-    this.page = 0
     this.memes = []
     this.keyword = ''
     this.loading = true
   }
   async init () {
-    for (let i = 0; i < 5; i++) {
-      this.memes = this.memes.concat(await MemeAPI.getByPopular(this.page))
-      this.page += 1
+    for (let page = 0; page < 5; page++) {
+      this.memes = this.memes.concat(await MemeAPI.getByPopular(page))
       this.loading = false
     }
   }
   searchByKeyword = async () => {
     this.loading = true
-    this.page = 0
     this.memes = []
     if (!this.keyword) {
       return this.init()
     }
-    for (let i = 0; i < 5; i++) {
-      const memes = this.memes.concat(await MemeAPI.getByKeyword(this.keyword, 0))
-      this.memes = memes.filter((meme, index, array) =>
-        index === array.findIndex((t) => (
-          t.generatorID === meme.generatorID
-        ))
-      )
-      this.page += 1
-      this.loading = false
-    }
+    this.memes = this.memes.concat(await MemeAPI.getByKeyword(this.keyword, 0))
+    this.loading = false
   }
 }
